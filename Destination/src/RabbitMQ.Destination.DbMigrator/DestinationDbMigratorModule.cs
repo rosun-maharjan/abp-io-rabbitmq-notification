bf2605e49a692d5cf6510e6f37411c73c9ad6f2a
Roshan Maharjan <rosun.maharjan@gmail.com>
2021-09-29 12:15:13 +0545

    Initial Commit

---
+++ Destination/src/RabbitMQ.Destination.DbMigrator/DestinationDbMigratorModule.cs
@@ -0,0 +1,20 @@
+﻿using RabbitMQ.Source.EntityFrameworkCore;
+using Volo.Abp.Autofac;
+using Volo.Abp.BackgroundJobs;
+using Volo.Abp.Modularity;
+
+namespace RabbitMQ.Destination.DbMigrator
+{
+    [DependsOn(
+        typeof(AbpAutofacModule),
+        typeof(SourceEntityFrameworkCoreModule),
+        typeof(DestinationApplicationContractsModule)
+        )]
+    public class DestinationDbMigratorModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            Configure<AbpBackgroundJobOptions>(options => options.IsJobExecutionEnabled = false);
+        }
+    }
+}
